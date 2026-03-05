0f41dffd8c64765b49a82fddc9e3f7a02dfc0326
Theo <temaxi36@gmail.com>
2026-03-05 11:44:16 +0100

    i'm back

--- src/main/java/ch/heig/gre/groupX/DfsGenerator.java
+++ src/main/java/ch/heig/gre/groupX/DfsGenerator.java
@@ -12,6 +12,59 @@ import java.util.*;
 public final class DfsGenerator implements MazeGenerator {
   @Override
   public void generate(MazeBuilder builder, int from) {
+    Graph g = builder.topology();
+    List<Boolean> visited = new ArrayList<>(Collections.nCopies(g.nbVertices(), false));
+    Stack<Integer> buffer = new Stack<>();
+
+    // add start point
+    buffer.push(0);
+
+    while (!buffer.empty()){
+
+      int i = buffer.peek();
+
+      if(visited.get(i)){
+        // mark the vertex as finish
+        buffer.pop();
+      }
+
+      // get neighbors
+      //List<Integer> neighbors = g.neighbors(i); //Arrays.asList(g.neighbors(i).);
+
+      // peek random neighbor
+      int j;
+      do {
+        j = (int) (Math.random() * neighbors.length);
+      }
+      while();
+
+
+      // destroy the wall
+
+
+      // mark i as visited
+
+
+
+
+    }
+
+    for (int i = 0; i < g.nbVertices(); i++) {
+      // get neighbors
+      int[] neighbors = g.neighbors(i);
+
+      // peek random neighbor
+
+
+      // destroy the wall
+
+
+      // mark i as visited
+
+      //
+
+    }
+    System.out.println("test");
     // Mise à jour de l'interface graphique :
     // builder.progressions().setLabel(..., ...);
   }
